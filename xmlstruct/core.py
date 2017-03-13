29b3cc9b380902ca9d63ae8793a8b5e5a74819ce
Matan Perelman <matan1008@gmail.com>
2017-03-13 22:03:45 +0200

    more features added to core, like FormatElement

--- xmlstruct/core.py
+++ xmlstruct/core.py
@@ -1,4 +1,5 @@
 from xml.etree import ElementTree
+from abc import ABCMeta, abstractmethod
 from xml_element import XmlElement
 from container import Container
 
@@ -23,3 +24,87 @@ class Struct(XmlElement):
         for child, subelement in zip(self.children, element):
             obj[subelement.tag] = subelement._parse(child)
         return obj
+
+
+class FormatElement(XmlElement):
+    """
+    A leaf element that requires formatting (any leaf when thinking about it)
+
+    The element is parsed and built with parse_func and build_func accordingly
+    """
+    __metaclass__ = ABCMeta
+
+    def  __init__(self, tag, attrib):
+        XmlElement.__init__(tag, attrib)
+
+    @abstractmethod
+    def parse_func(self, text):
+        """
+        This function parses element text
+
+        It return any wanted value
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
+    def build_func(self, value):
+        """
+        This function "builds" a value
+
+        The function gets the value and returns string
+        """
+        raise NotImplementedError()
+
+    def _build(self, obj):
+        element = ElementTree.Element(self.tag, self.attrib)
+        element.text = self.build_func(obj)
+        return element
+
+    def _parse(self, element):
+        return self.parse_func(element.text)
+
+
+class String(FormatElement):
+    """
+    A FormatElement that for string
+
+    Sounds unnecessary, but makes the project cleaner and clearer
+    """
+    def build_func(self, value):
+        return value
+
+    def parse_func(self, text):
+        return text
+
+
+class Int(FormatElement):
+    """
+    A FormatElement that for int
+    """
+    def build_func(self, value):
+        return str(value)
+
+    def parse_func(self, text):
+        return int(text)
+
+
+class Float(FormatElement):
+    """
+    A FormatElement that for float
+    """
+    def build_func(self, value):
+        return str(value)
+
+    def parse_func(self, text):
+        return float(text)
+
+
+class Hex(FormatElement):
+    """
+    A FormatElement that for hexadecimal, receives int as value
+    """
+    def build_func(self, value):
+        return "%X" % value
+
+    def parse_func(self, text):
+        return int(text, 16)
