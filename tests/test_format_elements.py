717d96d547172d5d3bcd109eb53fbbb1aff7861e
Matan Perelman <matan1008@gmail.com>
2017-03-13 22:38:43 +0200

    more testsgit add .!

--- tests/test_format_elements.py
+++ tests/test_format_elements.py
@@ -5,3 +5,15 @@ def test_int_build():
 
 def test_int_parse():
     assert Int("test", {"attr":"check"}).parse('<test attr="check">6</test>') == 6
+
+def test_float_build():
+    assert Float("test", {}).build(4.2) == '<test>4.2</test>'
+
+def test_float_parse():
+    assert Float("test", {}).parse('<test>4.2</test>') == 4.2
+
+def test_string_build():
+    assert String("ts", {}).build("or") == '<ts>or</ts>'
+
+def test_string_parse():
+    assert String("test", {}).parse('<ts>or</ts>') == "or"
