be1b0027ae7b9e11582e575cb977abd644146417
Matan Perelman <matan1008@gmail.com>
2017-03-14 20:55:46 +0200

    Container creation ugly bug fix

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -1,5 +1,6 @@
 __author__ = "Matan Perelman <matan1008@gmail.com>"
 
 from xmlstruct.core import Struct, Int, Float, Hex, String, FormatElement
+from xmlstruct.container import Container
 
-__all__ = ["Struct", "Int", "Float", "Hex", "String", "FormatElement"]
+__all__ = ["Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container"]
--- xmlstruct/container.py
+++ xmlstruct/container.py
@@ -6,12 +6,15 @@ class Container(OrderedDict):
 
     The Container may be used for both building and parsing
     """
+    def __init__(self, *args, **kwds):
+        OrderedDict.__init__(self, *args, **kwds)
+        # OrderedDict uses __setattr__ in his __init__, overriding it before
+        # here calls to _OrderedDict___root, thus raising AttributeError
+        self.__delattr__ = OrderedDict.__delitem__
+        self.__setattr__ = OrderedDict.__setitem__
 
     def __getattr__(self, name):
         try:
             return self[name]
         except KeyError:
             raise AttributeError(name)
-
-    __delattr__ = OrderedDict.__delitem__
-    __setattr__ = OrderedDict.__setitem__
