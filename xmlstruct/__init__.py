881b11e682d3f3cc119742eda913d52bd0607e54
Matan Perelman <matan1008@gmail.com>
2017-03-15 22:50:59 +0200

    Repeaters added to __all__

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -2,5 +2,9 @@ __author__ = "Matan Perelman <matan1008@gmail.com>"
 
 from xmlstruct.core import Struct, Int, Float, Hex, String, FormatElement
 from xmlstruct.container import Container
+from xmlstruct.elements.repeaters import Range, GreedyRange, Array
 
-__all__ = ["Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container"]
+__all__ = [
+    "Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container",
+    "Range", "GreedyRange", "Array"
+]
