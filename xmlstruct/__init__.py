8a2f998ca9b72db815e706d261b306096fa631e2
Matan Perelman <matan1008@gmail.com>
2017-03-17 13:14:22 +0200

    OrderedStruct and OrderedPairContainer added to __all__

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -1,10 +1,10 @@
-from xmlstruct.core import Struct
+from xmlstruct.core import Struct, OrderedStruct
 from xmlstruct.elements.formatters import Int, Float, Hex, String, FormatElement
-from xmlstruct.container import Container
+from xmlstruct.container import Container, OrderedPairContainer
 from xmlstruct.elements.repeaters import Range, GreedyRange, Array
 
 __all__ = [
     "Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container",
-    "Range", "GreedyRange", "Array"
+    "Range", "GreedyRange", "Array", "OrderedStruct", "OrderedPairContainer"
 ]
 __author__ = "Matan Perelman <matan1008@gmail.com>"
