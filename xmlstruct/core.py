cc7c69ed81c42a185a8f13901a5d498758cd260a
Matan Perelman <matan1008@gmail.com>
2017-03-17 12:54:45 +0200

    OrderedStruct added

--- xmlstruct/core.py
+++ xmlstruct/core.py
@@ -1,13 +1,15 @@
 from xml.etree import ElementTree
 from xmlstruct.xml_element import XmlElement
 from xmlstruct.container import Container
+from xmlstruct.exceptions import TagMismatchError
 
 
 class Struct(XmlElement):
     """
-    A sequence of xml elements.
+    A sequence of unordered xml elements.
 
-    The elements are parsed and built and the order they are defined
+    The elements are parsed and built according to the order they are given
+    in the __init__, without validating the order against the xml element 
     """
 
     def __init__(self, tag, attrib, *children):
@@ -24,6 +26,37 @@ class Struct(XmlElement):
     def _parse(self, element):
         obj = Container()
         obj[self.tag] = Container()
-        for child, subelement in zip(self.children, element):
+        for child in self.children:
+            obj[self.tag].update(child._parse(element.find(child.tag)))
+        return obj
+
+
+class OrderedStruct(XmlElement):
+    """
+    A sequence of ordered xml elements.
+
+    The elements are parsed and built according to the order they are given
+    in the __init__, with validating the order against the xml element
+    """
+
+    def __init__(self, tag, attrib, *children):
+        XmlElement.__init__(self, tag, attrib)
+        self.children = children
+
+    def _build(self, obj):
+        element = ElementTree.Element(self.tag, self.attrib)
+        for index, child in enumerate(self.children):
+            if child.tag != obj[self.tag][index][0]:
+                raise TagMismatchError()
+            sub_obj = Container({child.tag: obj[self.tag][index][1]})
+            element.append(child._build(sub_obj))
+        return element
+
+    def _parse(self, element):
+        obj = Container()
+        obj[self.tag] = Container()
+        for child, subelement in zip(self.children, list(element)):
+            if child.tag != subelement.tag:
+                raise TagMismatchError()
             obj[self.tag].update(child._parse(subelement))
         return obj
