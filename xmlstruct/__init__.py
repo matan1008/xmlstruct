d5eefefd714ef8ec70fdd6dcfe9fdb59aa665b79
Matan Perelman <matan1008@gmail.com>
2017-03-18 23:20:21 +0200

    ListContainer and ValueContainer added to __all__

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -1,10 +1,11 @@
 from xmlstruct.core import Struct, OrderedStruct
 from xmlstruct.elements.formatters import Int, Float, Hex, String, FormatElement
-from xmlstruct.container import Container, OrderedPairContainer
+from xmlstruct.container import Container, OrderedPairContainer, ListContainer, ValueContainer
 from xmlstruct.elements.repeaters import Range, GreedyRange, Array
 
 __all__ = [
     "Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container",
-    "Range", "GreedyRange", "Array", "OrderedStruct", "OrderedPairContainer"
+    "Range", "GreedyRange", "Array", "OrderedStruct", "OrderedPairContainer",
+    "ListContainer", "ValueContainer"
 ]
 __author__ = "Matan Perelman <matan1008@gmail.com>"
