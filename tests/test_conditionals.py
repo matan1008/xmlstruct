2ee62566a65bf7b715702d918486706a6a6a8bd9
Matan Perelman <matan1008@gmail.com>
2017-03-20 22:26:56 +0200

    More tests for optional

--- tests/test_conditionals.py
+++ tests/test_conditionals.py
@@ -1,4 +1,4 @@
-from xmlstruct import Int, Optional, ValueContainer
+from xmlstruct import Int, Optional, ValueContainer, Struct, Container
 
 def test_optional_int_build():
     obj = ValueContainer(6, xml_attrib={"attr": "check"})
@@ -18,3 +18,23 @@ def test_optional_int_build_none():
 def test_optional_int_parse_none():
     obj = None
     assert Optional(Int("test"))._parse(None) == obj
+
+
+def test_optional_recursive_build_none():
+    xml_struct = Struct(
+            "test",
+            Int("testint"),
+            Optional(Int("optestint"))
+    )
+    obj = Container({"testint": 3}, xml_attrib={"attr": "attrv"})
+    assert xml_struct.build(obj) == '<test attr="attrv"><testint>3</testint></test>'
+
+
+def test_optional_recursive_parse_none():
+    xml_struct = Struct(
+            "test",
+            Int("testint"),
+            Optional(Int("optestint"))
+    )
+    obj = Container({"testint": 3, "optestint":None}, xml_attrib={"attr": "attrv"})
+    assert xml_struct.parse('<test attr="attrv"><testint>3</testint></test>') == obj
