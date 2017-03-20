e8bc8990a41e108e26ad812d4785e4bc3cf89aca
Matan Perelman <matan1008@gmail.com>
2017-03-20 21:47:15 +0200

    Optional element added

---
+++ xmlstruct/elements/conditionals.py
@@ -0,0 +1,27 @@
+from xml.etree import ElementTree
+from xmlstruct.xml_element import XmlElement
+from xmlstruct.container import Container
+
+
+class Optional(XmlElement):
+    """
+    An optional element.
+    In _build, if obj is None don't build the element.
+    In _parse, if element is None don't parse the element
+    """
+
+    def __init__(self, child):
+        XmlElement.__init__(self, child.tag, child.attrib)
+        self.child = child
+
+    def _build(self, obj):
+        if obj is None:
+            return ElementTree.Element(None)
+        else:
+            return self.child._build(obj)
+
+    def _parse(self, element):
+        if element is None:
+            return Container()
+        else:
+            return self.child._parse(element)
