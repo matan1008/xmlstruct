bb99efd9ff731a95fbd983605b60b0760e7b0ec5
Matan Perelman <matan1008@gmail.com>
2017-03-25 22:10:14 +0300

    Switch added

--- xmlstruct/elements/conditionals.py
+++ xmlstruct/elements/conditionals.py
@@ -1,5 +1,7 @@
 from xml.etree import ElementTree
 from xmlstruct.xml_element import XmlElement
+from xmlstruct.container import ValueContainer
+from xmlstruct.exceptions import SwitchNoMatchError, SwitchSeveralMatchError
 
 
 class Optional(XmlElement):
@@ -35,8 +37,60 @@ class Switch(XmlElement):
     an Exception
     """
 
-    def __init__(self, tag, keyfunc, cases, default=None, **kwargs):
-        XmlElement.__init__(self, tag, kwargs.get("attrib"))
+    def __init__(self, keyfunc, cases, default=None, **kwargs):
+        XmlElement.__init__(self, None, kwargs.get("attrib"))
         self.keyfunc = keyfunc
         self.cases = cases
         self.default = default
+
+    def get_key(self, obj):
+        return self.keyfunc(obj) if callable(self.keyfunc) else self.keyfunc
+
+    def all_cases(self):
+        if self.default is None:
+            return self.cases.values()
+        else:
+            return self.cases.values() + [self.default]
+
+    def get_tag(self, parent):
+        case_tag = []
+        for case in self.all_cases():
+            if type(parent) == ElementTree.Element:
+                # for parsing
+                value_in_parent = parent.find(case.get_tag(parent))
+            else:
+                value_in_parent = parent.get(case.get_tag(parent))
+            if value_in_parent is not None:
+                case_tag.append(case.get_tag(parent))
+        if len(case_tag) == 0:
+            raise SwitchNoMatchError()
+        elif len(case_tag) == 1:
+            return case_tag[0]
+        else:
+            raise SwitchSeveralMatchError()
+
+    def _build(self, obj):
+        case = self.cases.get(self.get_key(obj), self.default)
+        if case is not None:
+            return case._build(obj)
+        else:
+            raise SwitchNoMatchError()
+
+    def _parse(self, element):
+        possible_objs = []
+        for case in self.all_cases():
+            try:
+                obj = case._parse(element)
+                if type(obj) == ValueContainer:
+                    obj = obj.value
+                if self.cases.get(self.get_key(obj), self.default) == case:
+                    if ElementTree.tostring(element) == case.build(obj):
+                        possible_objs.append(obj)
+            except Exception:
+                continue
+        if len(possible_objs) == 0:
+            raise SwitchNoMatchError()
+        elif len(possible_objs) == 1:
+            return possible_objs[0]
+        else:
+            raise SwitchSeveralMatchError()
