823f42cab9607ecfdd9b84b82cacc5d1b1bfe783
Matan Perelman <matan1008@gmail.com>
2017-03-25 22:11:10 +0300

    Switch added to __all__

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -2,11 +2,11 @@ from xmlstruct.core import Struct, OrderedStruct
 from xmlstruct.elements.formatters import Int, Float, Hex, String, FormatElement
 from xmlstruct.container import Container, OrderedPairContainer, ListContainer, ValueContainer
 from xmlstruct.elements.repeaters import Range, GreedyRange, Array
-from xmlstruct.elements.conditionals import Optional
+from xmlstruct.elements.conditionals import Optional, Switch
 
 __all__ = [
     "Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container",
     "Range", "GreedyRange", "Array", "OrderedStruct", "OrderedPairContainer",
-    "ListContainer", "ValueContainer"
+    "ListContainer", "ValueContainer", "Switch"
 ]
 __author__ = "Matan Perelman <matan1008@gmail.com>"
