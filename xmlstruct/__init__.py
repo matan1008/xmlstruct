79428dfd2269134d3995d326316062be1f9619bf
Matan Perelman <matan1008@gmail.com>
2017-03-28 19:37:37 +0300

    IfThenElse added to __all__

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -3,11 +3,11 @@ from xmlstruct.core import Struct, OrderedStruct
 from xmlstruct.elements.formatters import Int, Float, Hex, String, FormatElement
 from xmlstruct.container import Container, OrderedPairContainer, ListContainer, ValueContainer
 from xmlstruct.elements.repeaters import Range, GreedyRange, Array
-from xmlstruct.elements.conditionals import Optional, Switch
+from xmlstruct.elements.conditionals import Optional, Switch, IfThenElse
 
 __all__ = [
     "Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container",
     "Range", "GreedyRange", "Array", "OrderedStruct", "OrderedPairContainer",
-    "ListContainer", "ValueContainer", "Switch"
+    "ListContainer", "ValueContainer", "Switch", "IfThenElse"
 ]
 __author__ = "Matan Perelman <matan1008@gmail.com>"
