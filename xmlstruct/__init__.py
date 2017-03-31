a08ebdce4439504e7fbb1518069e187fe05535ff
Matan Perelman <matan1008@gmail.com>
2017-03-31 12:07:05 +0300

    Pass added to __all__

--- xmlstruct/__init__.py
+++ xmlstruct/__init__.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-from xmlstruct.core import Struct, OrderedStruct
+from xmlstruct.core import Struct, OrderedStruct, Pass
 from xmlstruct.elements.formatters import Int, Float, Hex, String, FormatElement
 from xmlstruct.container import Container, OrderedPairContainer, ListContainer, ValueContainer
 from xmlstruct.elements.repeaters import Range, GreedyRange, Array
@@ -8,6 +8,6 @@ from xmlstruct.elements.conditionals import Optional, Switch, IfThenElse
 __all__ = [
     "Struct", "Int", "Float", "Hex", "String", "FormatElement", "Container",
     "Range", "GreedyRange", "Array", "OrderedStruct", "OrderedPairContainer",
-    "ListContainer", "ValueContainer", "Switch", "IfThenElse"
+    "ListContainer", "ValueContainer", "Switch", "IfThenElse", "Pass"
 ]
 __author__ = "Matan Perelman <matan1008@gmail.com>"
